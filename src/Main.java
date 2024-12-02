8b1140034b7d171ee2dc4522637d5d4339e072c4
Eliseu <elimarmon@alu.edu.gva.es>
2024-12-02 09:21:12 +0100

    Exercici operadors programació

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.print("Introduzca el primer número: ");
+        num1 = input.nextInt(); input.nextLine();
+        System.out.print("Introduzca el segundo número: ");
+        num2 = input.nextInt(); input.nextLine();
+
+        System.out.printf("El resultado de la suma es: ");
+
+        // suma resta producto y potencia y division
+
+    }
+}
