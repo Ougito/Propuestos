9ea69ef727a968a14771fad9eef25c1950d5d8de
Eliseu <elimarmon@alu.edu.gva.es>
2024-12-02 10:02:59 +0100

    Añadidas resta, multiplicación, división y potencia

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,22 @@ public class Main {
         num1 = input.nextInt(); input.nextLine();
         System.out.print("Introduzca el segundo número: ");
         num2 = input.nextInt(); input.nextLine();
+
+        // suma y resta
+
         int suma = num1 + num2;
-
-        // suma resta producto y potencia y division
+        int resta = num1 - num2;
         System.out.println("El resultado de la suma es: " + suma);
+        System.out.println("El resultado de la resta es: " + resta);
 
+        // producto, potencia y división
+
+        int producto = num1 * num2;
+        double potencia =  Math.pow(num1, num2);
+        int division = num1/num2;
+        System.out.println("El resultado del producto es: " + producto);
+        System.out.println("El resultado de la potencia es: " + potencia);
+        System.out.println("El resultado de la division es: " + division);
     }
+
 }
