5c76bd01a934fbbe93c1a721fa644a85d4c40dc7
Eliseu <elimarmon@alu.edu.gva.es>
2024-12-02 13:30:32 +0100

    Ejercicio2 terminado

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.print("Introduce el primer número: ");
+        num1 = input.nextInt();input.nextLine();
+        System.out.print("Introduce el segundo número: ");
+        num2 = input.nextInt();input.nextLine();
+
+        if (num1 == num2) {
+            System.out.printf("El número %d y el número %d son iguales.", num1, num2);
+        } else {
+            System.out.printf("El número %d y el número %d son diferentes.", num1, num2);
+        }
+    }
 }
