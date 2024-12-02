f7516f4899d00d7f691339acb2e8064006ee4851
Eliseu <elimarmon@alu.edu.gva.es>
2024-12-02 12:39:04 +0100

    Añadir comentarios y formatear

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         int num1, num2;
+
+        // Introducción de datos
 
         System.out.print("Introduzca el primer número: ");
         num1 = input.nextInt(); input.nextLine();
         System.out.print("Introduzca el segundo número: ");
         num2 = input.nextInt(); input.nextLine();
 
-        // suma y resta
+        // Cálculo de la suma y la resta
 
         int suma = num1 + num2;
         int resta = num1 - num2;
         System.out.println("El resultado de la suma es: " + suma);
         System.out.println("El resultado de la resta es: " + resta);
 
-        // producto, potencia y división
+        // Cálculo del producto, la potencia y la división
 
         int producto = num1 * num2;
         double potencia =  Math.pow(num1, num2);
@@ -27,5 +30,4 @@ public class Main {
         System.out.println("El resultado de la potencia es: " + potencia);
         System.out.println("El resultado de la division es: " + division);
     }
-
 }
