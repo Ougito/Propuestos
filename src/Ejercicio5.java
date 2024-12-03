4cd460e80c73f853f4caeebcb5412bb6f3883a0f
Eliseu <elimarmon@alu.edu.gva.es>
2024-12-03 12:44:32 +0100

    Ejercicio 5 acabado

---
+++ src/Ejercicio5.java
@@ -0,0 +1,13 @@
+public class Ejercicio5 {
+    // Realiza un programa que muestre por pantalla los 20 primeros números naturales (1, 2, 3,…, 20)
+
+    public static void main(String[] args) {
+        for (int i = 1;i<=20;i++) {
+            System.out.print(i);
+            if (i < 20)
+                System.out.print(", ");
+            else
+                System.out.println(".");
+        }
+    }
+}
