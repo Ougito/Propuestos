3f6f4788748756342612a952859e711b67331706
Eliseu <elimarmon@alu.edu.gva.es>
2024-12-03 13:41:43 +0100

    bucle for sumando de 2 en 2

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -3,16 +3,14 @@ public class Ejercicio6 {
     // Realiza un programa que muestre los números pares comprendidos entre el 1 y el 200, sumando de 2 en 2
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 200; i++) {
-            if (i % 2 == 0) {
-                System.out.print(i);
-                if (i == 50 || i == 100 || i == 150)
-                    System.out.println(",");
-                else if (i < 200)
-                    System.out.print(", ");
-                else
-                    System.out.println(".");
-            }
+        for (int i = 2; i <= 200; i += 2 ) {
+            System.out.print(i);
+            if (i == 50 || i == 100 || i == 150)
+                System.out.println(",");
+            else if (i < 200)
+                System.out.print(", ");
+            else
+                System.out.println(".");
         }
     }
 }
